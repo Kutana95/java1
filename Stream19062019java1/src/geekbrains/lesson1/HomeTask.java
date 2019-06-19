e429b1d248eeeebaa8b870bbf59f1ccb44fd2ef7
kutana <anechkakutovaya@mail.ru>
2019-06-19 21:58:03 +0700

    Добавлена задача про високосный год

--- Stream19062019java1/src/geekbrains/lesson1/HomeTask.java
+++ Stream19062019java1/src/geekbrains/lesson1/HomeTask.java
@@ -46,12 +46,47 @@ public class HomeTask {
         System.out.println("Привет, " + name + "!");
     }
 
+    /*Написать метод, который определяет является ли год високосным.
+    Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    Для проверки работы вывести результаты работы метода в консоль
+     */
+
+    public static void leapYear(int year){
+
+        if (year <=0){
+            System.out.println("Year can't be negative!!!");
+        }else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0 & !(year / 100 % 4 == 0)) {
+                    System.out.println("Year " + year + " is not a leap year(");
+                } else {
+                    System.out.println("Year " + year + " is a leap year)");
+                }
+            } else {
+                System.out.println("Year " + year + " is not a leap year(");
+            }
+        }
+
+    }
+
     public static void main(String[] args){
 
         System.out.println(calculate(5, 10, 32, 96));
         System.out.println(between10and20(3, 5));
         isPositiveOrNegative(-52);
         privet("Ann");
+
+        leapYear(-2502);
+        leapYear(4);
+        leapYear(5);
+        leapYear(47);
+        leapYear(96);
+        leapYear(100);
+        leapYear(200);
+        leapYear(300);
+        leapYear(400);
+        leapYear(500);
+        leapYear(1200);
 
     }
 }
